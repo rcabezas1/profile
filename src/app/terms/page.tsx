b73901478f907ffdd87a4ee91fdae20e8cd82b67
Edwin Ricardo Romero Cabezas <rcabezas1@gmail.com>
2024-04-01 18:12:26 -0500

    adding terms and condition page

---
+++ src/app/terms/page.tsx
@@ -0,0 +1,130 @@
+
+export default function TermsCondition() {
+    return (
+        <div>
+            <p><strong>PRIVACY POLICY</strong></p>
+            <p><strong><strong>Last updated&nbsp;April 01, 2024</strong></strong></p>
+            <p>This privacy notice for&nbsp;Edwin Ricardo Romero Cabezas&nbsp;("<strong><strong>we</strong></strong>," "<strong><strong>us</strong></strong>," or "<strong><strong>our</strong></strong>"), describes how and why we might collect, store, use, and/or share ("<strong><strong>process</strong></strong>") your information when you use our services ("<strong><strong>Services</strong></strong>"), such as when you:</p>
+            <ul>
+                <li>Download and use&nbsp;our mobile application&nbsp;(PB Personal Budget)&nbsp;,&nbsp;or any other application of ours that links to this privacy notice</li>
+            </ul>
+            <ul>
+                <li>Engage with us in other related ways, including any sales, marketing, or events</li>
+            </ul>
+            <p><strong><strong>Questions or concerns?&nbsp;</strong></strong>Reading this privacy notice will help you understand your privacy rights and choices. If you do not agree with our policies and practices, please do not use our Services.&nbsp;If you still have any questions or concerns, please contact us at&nbsp;rcabezas1@gmail.com.</p>
+            <p><strong><strong>SUMMARY OF KEY POINTS</strong></strong></p>
+            <p><strong><strong><em><em>This summary provides key points from our privacy notice, but you can find out more details about any of these topics by clicking the link following each key point or by using our&nbsp;</em></em></strong></strong><strong><strong><em><em><a href="#toc">table of contents</a></em></em></strong></strong><strong><strong><em></em></strong></strong><strong><strong><em><em>below to find the section you are looking for.</em></em></strong></strong></p>
+            <p><strong><strong>What personal information do we process?</strong></strong>&nbsp;When you visit, use, or navigate our Services, we may process personal information depending on how you interact with us and the Services, the choices you make, and the products and features you use. Learn more about&nbsp;<a href="#personalinfo">personal information you disclose to us</a>.</p>
+            <p><strong><strong>Do we process any sensitive personal information?</strong></strong>&nbsp;We do not process sensitive personal information.</p>
+            <p><strong><strong>Do we receive any information from third parties?</strong></strong>&nbsp;We do not receive any information from third parties.</p>
+            <p><strong><strong>How do we process your information?</strong></strong>&nbsp;We process your information to provide, improve, and administer our Services, communicate with you, for security and fraud prevention, and to comply with law. We may also process your information for other purposes with your consent. We process your information only when we have a valid legal reason to do so. Learn more about&nbsp;<a href="#infouse">how we process your information</a>.</p>
+            <p><strong><strong>In what situations and with which&nbsp;parties do we share personal information?</strong></strong>&nbsp;We may share information in specific situations and with specific&nbsp;third parties. Learn more about&nbsp;<a href="#whoshare">when and with whom we share your personal information</a>.</p>
+            <p><strong><strong>How do we keep your information safe?</strong></strong>&nbsp;We have&nbsp;organizational&nbsp;and technical processes and procedures in place to protect your personal information. However, no electronic transmission over the internet or information storage technology can be guaranteed to be 100% secure, so we cannot promise or guarantee that hackers, cybercriminals, or other&nbsp;unauthorized&nbsp;third parties will not be able to defeat our security and improperly collect, access, steal, or modify your information. Learn more about&nbsp;<a href="#infosafe">how we keep your information safe</a>.</p>
+            <p><strong><strong>What are your rights?</strong></strong>&nbsp;Depending on where you are located geographically, the applicable privacy law may mean you have certain rights regarding your personal information. Learn more about&nbsp;<a href="#privacyrights">your privacy rights</a>.</p>
+            <p><strong><strong>How do you exercise your rights?</strong></strong>&nbsp;The easiest way to exercise your rights is by&nbsp;submitting a <a href="https://app.termly.io/notify/3f462ff5-61d4-45ca-a12c-7c87ded671c9">data subject access request</a>&nbsp;, or by contacting us. We will consider and act upon any request in accordance with applicable data protection laws.</p>
+            <p>Want to learn more about what we do with any information we collect?&nbsp;<a href="#toc">Review the privacy notice in full</a>.</p>
+            <p><strong><strong>TABLE OF CONTENTS</strong></strong></p>
+            <ol>
+                <li><a href="#infocollect"> WHAT INFORMATION DO WE COLLECT?</a></li>
+                <li><a href="#infouse"> HOW DO WE PROCESS YOUR INFORMATION?</a></li>
+                <li><a href="#whoshare"> WHEN AND WITH WHOM DO WE SHARE YOUR PERSONAL INFORMATION?</a></li>
+                <li><a href="#sociallogins"> HOW DO WE HANDLE YOUR SOCIAL LOGINS?</a></li>
+                <li><a href="#inforetain"> HOW LONG DO WE KEEP YOUR INFORMATION?</a></li>
+                <li><a href="#infosafe"> HOW DO WE KEEP YOUR INFORMATION SAFE?</a></li>
+                <li><a href="#privacyrights"> WHAT ARE YOUR PRIVACY RIGHTS?</a></li>
+                <li><a href="#DNT"> CONTROLS FOR DO-NOT-TRACK FEATURES</a></li>
+                <li><a href="#policyupdates"> DO WE MAKE UPDATES TO THIS NOTICE?</a></li>
+                <li><a href="#contact"> HOW CAN YOU CONTACT US ABOUT THIS NOTICE?</a></li>
+                <li><a href="#request"> HOW CAN YOU REVIEW, UPDATE, OR DELETE THE DATA WE COLLECT FROM YOU?</a></li>
+            </ol>
+            <ol>
+                <li><strong><strong> WHAT INFORMATION DO WE COLLECT?</strong></strong></li>
+            </ol>
+            <p><strong><strong>Personal information you disclose to us</strong></strong></p>
+            <p><strong><strong><em><em>In Short:</em></em></strong></strong><strong><strong><em></em></strong></strong><em><em>We collect personal information that you provide to us.</em></em></p>
+            <p>We collect personal information that you voluntarily provide to us when you&nbsp;register on the Services, express an interest in obtaining information about us or our products and Services, when you participate in activities on the Services, or otherwise when you contact us.</p>
+            <p><strong><strong>Personal Information Provided by You.</strong></strong>&nbsp;The personal information that we collect depends on the context of your interactions with us and the Services, the choices you make, and the products and features you use. The personal information we collect may include the following:</p>
+            <ul>
+                <li>email addresses</li>
+            </ul>
+            <ul>
+                <li>names</li>
+            </ul>
+            <p><strong><strong>Sensitive Information.</strong></strong>&nbsp;We do not process sensitive information.</p>
+            <p><strong><strong>Social Media Login Data.&nbsp;</strong></strong>We may provide you with the option to register with us using your existing social media account details, like your Facebook, Twitter, or other social media account. If you choose to register in this way, we will collect the information described in the section called&nbsp;"<a href="#sociallogins">HOW DO WE HANDLE YOUR SOCIAL LOGINS?</a>&nbsp;"&nbsp;below.</p>
+            <p><strong><strong>Application Data.</strong></strong>&nbsp;If you use our application(s), we also may collect the following information if you choose to provide us with access or permission:</p>
+            <ul>
+                <li><em><em>Mobile Device Access.</em></em>&nbsp;We may request access or permission to certain features from your mobile device, including your mobile device's&nbsp;sms messages,&nbsp;and other features. If you wish to change our access or permissions, you may do so in your device's settings.</li>
+            </ul>
+            <p>This information is primarily needed to maintain the security and operation of our application(s), for troubleshooting, and for our internal analytics and reporting purposes.</p>
+            <p>All personal information that you provide to us must be true, complete, and accurate, and you must notify us of any changes to such personal information.</p>
+            <ol >
+                <li><strong><strong> HOW DO WE PROCESS YOUR INFORMATION?</strong></strong></li>
+            </ol>
+            <p><strong><strong><em><em>In Short:&nbsp;</em></em></strong></strong><em><em>We process your information to provide, improve, and administer our Services, communicate with you, for security and fraud prevention, and to comply with law. We may also process your information for other purposes with your consent.</em></em></p>
+            <p><strong><strong>We process your personal information for a variety of reasons, depending on how you interact with our Services, including:</strong></strong></p>
+            <ul>
+                <li><strong><strong>To facilitate account creation and authentication and otherwise manage user accounts.&nbsp;</strong></strong>We may process your information so you can create and log in to your account, as well as keep your account in working order.</li>
+            </ul>
+            <ol >
+                <li><strong><strong> WHEN AND WITH WHOM DO WE SHARE YOUR PERSONAL INFORMATION?</strong></strong></li>
+            </ol>
+            <p><strong><strong><em><em>In Short:</em></em></strong></strong><em><em>&nbsp;We may share information in specific situations described in this section and/or with the following&nbsp;third parties.</em></em></p>
+            <p>We&nbsp;may need to share your personal information in the following situations:</p>
+            <ul>
+                <li><strong><strong>Business Transfers.</strong></strong>&nbsp;We may share or transfer your information in connection with, or during negotiations of, any merger, sale of company assets, financing, or acquisition of all or a portion of our business to another company.</li>
+            </ul>
+            <ol >
+                <li><strong><strong> HOW DO WE HANDLE YOUR SOCIAL LOGINS?</strong></strong></li>
+            </ol>
+            <p><strong><strong><em><em>In Short:&nbsp;</em></em></strong></strong><em><em>If you choose to register or log in to our Services using a social media account, we may have access to certain information about you.</em></em></p>
+            <p>Our Services offer you the ability to register and log in using your third-party social media account details (like your Facebook or Twitter logins). Where you choose to do this, we will receive certain profile information about you from your social media provider. The profile information we receive may vary depending on the social media provider concerned, but will often include your name, email address, friends list, and profile picture, as well as other information you choose to make public on such a social media platform.</p>
+            <p>We will use the information we receive only for the purposes that are described in this privacy notice or that are otherwise made clear to you on the relevant Services. Please note that we do not control, and are not responsible for, other uses of your personal information by your third-party social media provider. We recommend that you review their privacy notice to understand how they collect, use, and share your personal information, and how you can set your privacy preferences on their sites and apps.</p>
+            <ol >
+                <li><strong><strong> HOW LONG DO WE KEEP YOUR INFORMATION?</strong></strong></li>
+            </ol>
+            <p><strong><strong><em><em>In Short:&nbsp;</em></em></strong></strong><em><em>We keep your information for as long as necessary to&nbsp;fulfill&nbsp;the purposes outlined in this privacy notice unless otherwise required by law.</em></em></p>
+            <p>We will only keep your personal information for as long as it is necessary for the purposes set out in this privacy notice, unless a longer retention period is required or permitted by law (such as tax, accounting, or other legal requirements).&nbsp;No purpose in this notice will require us keeping your personal information for longer than the period of time in which users have an account with us&nbsp;. </p>
+            <p>When we have no ongoing legitimate business need to process your personal information, we will either delete or&nbsp;anonymize&nbsp;such information, or, if this is not possible (for example, because your personal information has been stored in backup archives), then we will securely store your personal information and isolate it from any further processing until deletion is possible.</p>
+            <ol >
+                <li><strong><strong> HOW DO WE KEEP YOUR INFORMATION SAFE?</strong></strong></li>
+            </ol>
+            <p><strong><strong><em><em>In Short:&nbsp;</em></em></strong></strong><em><em>We aim to protect your personal information through a system of&nbsp;organizational&nbsp;and technical security measures.</em></em></p>
+            <p>We have implemented appropriate and reasonable technical and&nbsp;organizational&nbsp;security measures designed to protect the security of any personal information we process. However, despite our safeguards and efforts to secure your information, no electronic transmission over the Internet or information storage technology can be guaranteed to be 100% secure, so we cannot promise or guarantee that hackers, cybercriminals, or other&nbsp;unauthorized&nbsp;third parties will not be able to defeat our security and improperly collect, access, steal, or modify your information. Although we will do our best to protect your personal information, transmission of personal information to and from our Services is at your own risk. You should only access the Services within a secure environment.</p>
+            <ol >
+                <li><strong><strong> WHAT ARE YOUR PRIVACY RIGHTS?</strong></strong></li>
+            </ol>
+            <p><strong><strong><em><em>In Short:</em></em></strong></strong><em><em> You may review, change, or terminate your account at any time.</em></em></p>
+            <p><strong><strong><u>Withdrawing your consent:</u></strong></strong>&nbsp;If we are relying on your consent to process your personal information,&nbsp;which may be express and/or implied consent depending on the applicable law,&nbsp;you have the right to withdraw your consent at any time. You can withdraw your consent at any time by contacting us by using the contact details provided in the section&nbsp;"&nbsp;<a href="#contact">HOW CAN YOU CONTACT US ABOUT THIS NOTICE?</a>&nbsp;"&nbsp;below&nbsp;.</p>
+            <p>However, please note that this will not affect the lawfulness of the processing before its withdrawal nor,&nbsp;when applicable law allows,&nbsp;will it affect the processing of your personal information conducted in reliance on lawful processing grounds other than consent.</p>
+            <p><strong><strong>Account Information</strong></strong></p>
+            <p>If you would at any time like to review or change the information in your account or terminate your account, you can:</p>
+            <ul>
+                <li>Log in to your account settings and update your user account.</li>
+            </ul>
+            <p>Upon your request to terminate your account, we will deactivate or delete your account and information from our active databases. However, we may retain some information in our files to prevent fraud, troubleshoot problems, assist with any investigations, enforce our legal terms and/or comply with applicable legal requirements.</p>
+            <p>If you have questions or comments about your privacy rights, you may email us at&nbsp;rcabezas1@gmail.com&nbsp;.</p>
+            <ol >
+                <li><strong><strong> CONTROLS FOR DO-NOT-TRACK FEATURES</strong></strong></li>
+            </ol>
+            <p>Most web browsers and some mobile operating systems and mobile applications include a Do-Not-Track (&nbsp;"DNT"&nbsp;) feature or setting you can activate to signal your privacy preference not to have data about your online browsing activities monitored and collected. At this stage no uniform technology standard for&nbsp;recognizing&nbsp;and implementing DNT signals has been&nbsp;finalized&nbsp;. As such, we do not currently respond to DNT browser signals or any other mechanism that automatically communicates your choice not to be tracked online. If a standard for online tracking is adopted that we must follow in the future, we will inform you about that practice in a revised version of this privacy notice.</p>
+            <ol >
+                <li><strong><strong> DO WE MAKE UPDATES TO THIS NOTICE?</strong></strong></li>
+            </ol>
+            <p><em><em><strong><strong>In Short:&nbsp;</strong></strong></em></em><em><em>Yes, we will update this notice as necessary to stay compliant with relevant laws.</em></em></p>
+            <p>We may update this privacy notice from time to time. The updated version will be indicated by an updated&nbsp;"Revised"&nbsp;date and the updated version will be effective as soon as it is accessible. If we make material changes to this privacy notice, we may notify you either by prominently posting a notice of such changes or by directly sending you a notification. We encourage you to review this privacy notice frequently to be informed of how we are protecting your information.</p>
+            <ol >
+                <li><strong><strong> HOW CAN YOU CONTACT US ABOUT THIS NOTICE?</strong></strong></li>
+            </ol>
+            <p>If you have questions or comments about this notice, you may&nbsp;email us at&nbsp;rcabezas1@gmail.com or contact us by post at:</p>
+            <p>Edwin Ricardo Romero Cabezas</p>
+            <p>__________</p>
+            <p>__________</p>
+            <ol >
+                <li><strong><strong> HOW CAN YOU REVIEW, UPDATE, OR DELETE THE DATA WE COLLECT FROM YOU?</strong></strong></li>
+            </ol>
+            <p>Based on the applicable laws of your country, you may have the right to request access to the personal information we collect from you, change that information, or delete it.&nbsp;To request to review, update, or delete your personal information, please&nbsp;fill out and submit a <a href="https://app.termly.io/notify/3f462ff5-61d4-45ca-a12c-7c87ded671c9">data subject access request</a>&nbsp;.</p>
+            <p>This privacy policy was created using Termly's&nbsp;<a href="https://termly.io/products/privacy-policy-generator/">Privacy Policy Generator</a>.</p>
+        </div>
+    );
+}
